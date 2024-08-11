ae213bb5b5afb893af883d7284bd98bc29f6b2d1
Saki Ota <saki@SakinoMacBook-Air.local>
2024-08-11 20:44:05 +1000

    complete mobile menu

--- 05_JavaScriptを実践で使う/050_モバイル用メニューを作成しよう/start/main.js
+++ 05_JavaScriptを実践で使う/050_モバイル用メニューを作成しよう/start/main.js
@@ -1,24 +1,25 @@
-class MobileMenu{
-    constructor(){
-    this.DOM={};
-    this.DOM.btn=document.querySelector('.mobile-menu__btn');
-    this.DOM.container=document.querySelector('#global-container');
-    this.eventType=this._getEventType();
+class MobileMenu {
+  constructor() {
+    this.DOM = {};
+    this.DOM.btn = document.querySelector(".mobile-menu__btn");
+    this.DOM.cover = document.querySelector(".mobile-menu__cover");
+    this.DOM.container = document.querySelector("#global-container");
+    this.eventType = this._getEventType();
     this._addEvent();
-  this.DOM.btn.addEventListener('click',this._toggle.bind(this));
-}
- 
- 
-_toggle(){
-    this.DOM.container.classList.toggle('menu-open');
-    }
- _addEvent(){
-       this.DOM.btn.addEventListener('click',this._toggle.bind(this))
-   }
-_getEventType() {
-   const isTouchCapable = "ontouchstart" in window;
+  }
 
-   return isTouchCapable ? "touchstart" : "click";
-}
+  _toggle() {
+    this.DOM.container.classList.toggle("menu-open");
+  }
+  _addEvent() {
+    this.DOM.btn.addEventListener("click", this._toggle.bind(this));
+    this.DOM.cover.addEventListener("click", this._toggle.bind(this));
+  }
+  _getEventType() {
+    const isTouchCapable = "ontouchstart" in window;
+
+    return isTouchCapable ? "touchstart" : "click";
+  }
 }
 new MobileMenu();
+
